6619f5383fbf609a96ac9559ce3591cba529e8f8
AyakaTakahashi <132245813+suco31a42@users.noreply.github.com>
2023-05-14 02:55:09 +0000

    [add]4-27　footer,top,about,sgin_upレイアウト完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :post_images, dependent: :destroy
   has_one_attached :image
-  
-  validates :name, uniqueness: true
+
+  validates :name, uniqueness: true, length: {minimum: 2, maximum: 20 }
   validates :email, uniqueness: true
-  #validates :password, uniqueness: true
 end
