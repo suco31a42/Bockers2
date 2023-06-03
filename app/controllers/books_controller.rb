3f49b38eddb93d595e6fc21478aa43c7aefd831a
AyakaTakahashi <132245813+suco31a42@users.noreply.github.com>
2023-06-03 08:51:04 +0000

    [finish]応用課題7a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   before_action :is_matching_login_user, only: [:edit, :update, :destroy]
+
   def new
     @book = Book.new
   end
@@ -11,16 +12,26 @@ class BooksController < ApplicationController
       flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
     else
-      @books = Book.all
-      @user = current_user
-      render :index
+      @books = Book.includes(:favorited_users).
+        sort_by {|x|
+          x.favorited_users.includes(:favorites).where(created_at: from...to).size
+        }.reverse
+      render 'index'
     end
   end
 
   def index
-    @books = Book.all
-    @book = Book.new
-    @user = current_user
+    @book  = Book.new
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    @books = Book.left_outer_joins(:favorited_users)
+                .group('books.id')
+                .order('COUNT(favorites.id) DESC')
+                .select('books.*, COUNT(favorites.id) as favorites_count')
+                .where('favorites.created_at >= ?', from)
+                .where('favorites.created_at <= ?', to )
+                .or(Book.left_outer_joins(:favorited_users)
+                        .where(favorites: {id: nil}))
   end
 
   def show
