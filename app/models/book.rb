3f4c2d2ad2da48a61f50f9d2258ef9b8987fa46a
AyakaTakahashi <132245813+suco31a42@users.noreply.github.com>
2023-06-09 12:22:50 +0000

    [finish]応用課題8b

--- app/models/book.rb
+++ app/models/book.rb
@@ -11,10 +11,13 @@ class Book < ApplicationRecord
   end
   
   scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
   scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
   scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
-  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
-
-
-
+  scope :created_2day, -> { where(created_at: 2.day.ago.all_day) }
+  scope :created_3day, -> { where(created_at: 3.day.ago.all_day) }
+  scope :created_4day, -> { where(created_at: 4.day.ago.all_day) }
+  scope :created_5day, -> { where(created_at: 5.day.ago.all_day) }
+  scope :created_6day, -> { where(created_at: 6.day.ago.all_day) }
+  
 end
